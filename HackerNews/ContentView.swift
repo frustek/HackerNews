d58f014dcf2d0cec3a302507555cf6c54e5c4e86
Adam Mroz <MrozAdam1992@gmail.com>
2022-09-10 22:18:31 +0200

    List added

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -35,7 +35,7 @@ struct Post: Identifiable {
 
 let posts = [
     Post(id: 1, title: "Hello"),
-    Post(id: 2, title: "Hello"),
-    Post(id: 3, title: "Hello"),
-    Post(id: 4, title: "Hello"),
-    Post(id: 5, title: "Hello")]
+    Post(id: 2, title: "Bonjur"),
+    Post(id: 3, title: "Hola"),
+    Post(id: 4, title: "Servus"),
+    Post(id: 5, title: "Cześć")]
