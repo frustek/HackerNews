0400f0e4fa251cf7d44e54952fea51e90360b97c
Adam Mroz <MrozAdam1992@gmail.com>
2022-09-10 17:36:59 +0200

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Adam Mroz on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
