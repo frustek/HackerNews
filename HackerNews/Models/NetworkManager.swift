7cc4c543cd82c016f792c28283f85b5167f29443
Adam Mroz <MrozAdam1992@gmail.com>
2023-05-21 11:06:57 +0200

    URL change

---
+++ HackerNews/Models/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  HackerNews
+//
+//  Created by Adam Mroz on 10/09/2022.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    func fetchData(){
+        let url = URL(string: "https://hacker-news.firebaseio.com/v0/beststories.json")
+    }
+    
+}
